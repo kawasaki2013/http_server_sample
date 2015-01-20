674276809ae0c06105c096d8674526be16ac0473
kawasaki2013 <bluehighwayjp@yahoo.co.jp>
2015-01-20 14:53:25 +0900

    Update http_server.py
    Added  the  method that change to ASCII code. This code can be used  in version 3.x.

--- http_server.py
+++ http_server.py
@@ -1,28 +1,28 @@
-import SocketServer
+import socketserver
 
-class MyHTTPRequestHandler(SocketServer.StreamRequestHandler):
+class MyHTTPRequestHandler(socketserver.StreamRequestHandler):
 
     def handle(self):
         data = self.rfile.readline().strip()
-        print data
+        print(data)
 
         f = open("index.html", 'r')
 
-        self.wfile.write("HTTP/1.1 200 OK\r\n")
-        self.wfile.write("Content-Type: text/html; charset=utf-8\r\n")
-        self.wfile.write("\r\n")
-        self.wfile.write(f.read())
+        self.wfile.write("HTTP/1.1 200 OK\r\n".encode('ascii'))
+        self.wfile.write("Content-Type: text/html; charset=utf-8\r\n".encode('ascii'))
+        self.wfile.write("\r\n".encode('ascii'))
+        self.wfile.write(f.read().encode('ascii'))
 
         f.close()
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 0
 
-    server = SocketServer.TCPServer((HOST, PORT), MyHTTPRequestHandler)
+    server = socketserver.TCPServer((HOST, PORT), MyHTTPRequestHandler)
     ip, port = server.server_address
 
     ip, port = server.server_address
-    print "IP: %s" % ip
-    print "Port: %s" % port
+    print("IP: %s" % ip)
+    print("Port: %s" % port)
 
     server.serve_forever()
